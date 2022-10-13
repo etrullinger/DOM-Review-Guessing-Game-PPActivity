e61be33f473cfafd4474fbc84c6447d00e6a53ba
etrullinger <ellietrullinger@gmail.com>
2022-10-13 15:45:56 -0400

    Guessing Game Complete

---
+++ index.js
@@ -0,0 +1,29 @@
+let randomNumber = Math.floor(Math.random() * (100 - 1)) + 1;
+let output = document.querySelector("#output");
+let counter = 0;
+
+function numberMatch() {
+    let guess = document.querySelector("#guess").value;
+    if (counter < 5) {
+        if (guess != randomNumber) {
+            if (guess > randomNumber) {
+                output.textContent = "Guess again. Try a lower number.";
+                counter++;
+            } else {
+                output.textContent = "Guess again. Try a higher number.";
+                counter++;
+            }
+        } else {
+            output.textContent = "Congratulations YOU WIN!";
+        }
+    } else {
+        output.textContent = "Sorry you ran out of guesses. Better luck next time!";
+    }
+    
+}
+
+function reset() {
+    randomNumber = Math.floor(Math.random() * (100 - 1)) + 1;
+    output.textContent = "A new random number has been generated."
+    counter = 0;
+}
